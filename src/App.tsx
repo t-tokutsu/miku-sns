864b5f85bc3352ba6b8b5ebba5d3e884670a8b83
tokutsu <tokutsu.beeworks@gmail.com>
2023-07-08 13:53:18 +0900

    レイアウトの調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Avatar, Box, HStack, Spacer, Stack, Text } from "@chakra-ui/react";
 import { FC } from "react";
 import { Hooks } from "./features/player/Hooks";
 import { Layout } from "./layout/Layout";
@@ -6,7 +7,28 @@ import { Layout } from "./layout/Layout";
 export const App: FC = () => {
   return (
     <>
-      <Layout>テスト</Layout>
+      <Layout>
+        <Box>
+          {/* 投稿 */}
+          <HStack>
+            <Avatar />
+            <Box>
+              <Stack>
+                <HStack>
+                  <Text>アカウント名</Text>
+                  <Spacer />
+                  <Text>投稿時間</Text>
+                </HStack>
+                <Text>投稿内容</Text>
+                <HStack>
+                  <Text>いいね数</Text>
+                  <Text>コメント数</Text>
+                </HStack>
+              </Stack>
+            </Box>
+          </HStack>
+        </Box>
+      </Layout>
       <Hooks />
     </>
   );
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,8 +1,23 @@
-import { Box } from "@chakra-ui/react";
+import { Box, SimpleGrid, Stack } from "@chakra-ui/react";
 import { FC, ReactNode } from "react";
 
 export const Layout: FC<{
   children: ReactNode;
 }> = ({ children }) => {
-  return <Box>{children}</Box>;
+  return (
+    <SimpleGrid gridTemplateColumns={"auto 1fr"}>
+      {/* メニューエリア */}
+      <Stack w={"200px"}>
+        <Box>ロゴ</Box>
+        <Stack>
+          <Box>メニュー</Box>
+          <Box>メニュー</Box>
+          <Box>メニュー</Box>
+          <Box>メニュー</Box>
+        </Stack>
+      </Stack>
+      {/* メインエリア */}
+      <Box>{children}</Box>
+    </SimpleGrid>
+  );
 };
