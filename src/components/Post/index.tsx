bf5f5606622e3530f9cd59a057a40b3b3c4c55f2
tokutsu <tokutsu.beeworks@gmail.com>
2023-07-16 20:40:24 +0900

    表示エラーの修正

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -6,11 +6,10 @@ import {
   Spacer,
   Box,
   BoxProps,
-  Collapse,
   chakra,
   Center,
 } from "@chakra-ui/react";
-import { FC, useLayoutEffect, useState } from "react";
+import { FC } from "react";
 import { useJotaiAccountId } from "../../features/account/useJotaiAccountId";
 import { accountData } from "../../features/posts/data/accounts";
 import { TypePost } from "../../features/posts/data/posts";
@@ -25,71 +24,63 @@ export const Post: FC<{
 }> = ({ post, boxProps, isActive = false }) => {
   const { setAccountId } = useJotaiAccountId();
   const { id, accountId, date, content, hasImage } = post;
-  const [isShow, setIsShow] = useState(isActive ? true : false);
-  useLayoutEffect(() => {
-    setTimeout(() => {
-      setIsShow(true);
-    }, 100);
-  }, []);
   return (
-    <Collapse animateOpacity in={isShow}>
-      <Box
-        bg={
-          "linear-gradient(90deg, rgba(144, 245, 154, 1), rgba(4, 202, 255, 1))"
-        }
-        borderRadius={8}
-        id={post.id}
-        p={0.5}
-        {...boxProps}
+    <Box
+      bg={
+        "linear-gradient(90deg, rgba(144, 245, 154, 1), rgba(4, 202, 255, 1))"
+      }
+      borderRadius={8}
+      id={post.id}
+      p={0.5}
+      {...boxProps}
+    >
+      <HStack
+        alignItems={"flex-start"}
+        backdropFilter={"blur(12px)"}
+        bg={!isActive ? "white" : undefined}
+        borderRadius={6}
+        justifySelf={"stretch"}
+        key={id}
+        p={3}
       >
-        <HStack
-          alignItems={"flex-start"}
-          backdropFilter={"blur(12px)"}
-          bg={!isActive ? "white" : undefined}
-          borderRadius={6}
-          justifySelf={"stretch"}
-          key={id}
-          p={3}
-        >
-          <Avatar
-            onClick={() => setAccountId(accountId)}
-            size={"sm"}
-            src={`/images/accounts/${accountId}/icon.png`}
-          />
-          <Stack spacing={1} w={"full"}>
-            <HStack>
-              <Text
-                fontSize={"md"}
-                fontWeight={"bold"}
-                onClick={() => setAccountId(accountId)}
-              >
-                {accountData[accountId].name}
-              </Text>
-              <Spacer />
-              <PostDate date={date} />
+        <Avatar
+          onClick={() => setAccountId(accountId)}
+          size={"sm"}
+          src={`/images/accounts/${accountId}/icon.png`}
+        />
+        <Stack spacing={1} w={"full"}>
+          <HStack>
+            <Text
+              fontSize={"md"}
+              fontWeight={"bold"}
+              onClick={() => setAccountId(accountId)}
+            >
+              {accountData[accountId].name}
+            </Text>
+            <Spacer />
+            <PostDate date={date} />
+          </HStack>
+          <Stack>
+            <Text fontSize={"sm"} whiteSpace={"pre-wrap"}>
+              {content}
+            </Text>
+            {/* 画像 */}
+            {hasImage && (
+              <Center>
+                <chakra.img
+                  alt=""
+                  maxH={"300px"}
+                  src={`/images/posts/${id}.png`}
+                />
+              </Center>
+            )}
+            <HStack spacing={4}>
+              <Likes post={post} />
+              <Replies isActive={isActive} post={post} />
             </HStack>
-            <Stack>
-              <Text fontSize={"sm"} whiteSpace={"pre-wrap"}>
-                {content}
-              </Text>
-              {/* 画像 */}
-              {hasImage && (
-                <Center>
-                  <chakra.img
-                    alt=""
-                    maxH={"300px"}
-                    src={`/images/posts/${id}.png`}
-                  />
-                </Center>
-              )}
-              <HStack spacing={4}>
-                <Likes post={post} />
-                <Replies isActive={isActive} post={post} />
-              </HStack>
-            </Stack>
           </Stack>
-        </HStack>
-      </Box>
-    </Collapse>
+        </Stack>
+      </HStack>
+    </Box>
   );
 };
