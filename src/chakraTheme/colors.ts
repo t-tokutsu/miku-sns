8e2b98245a6794b37966f73cc1a89a5e76a3dbb4
tokutsu <tokutsu.beeworks@gmail.com>
2023-07-16 21:48:51 +0900

    ローディングの追加

--- src/chakraTheme/colors.ts
+++ src/chakraTheme/colors.ts
@@ -5,8 +5,7 @@
  */
 export const colors = {
   gradation: {
-    green:
-      "linear-gradient(90deg, rgba(144, 245, 154, 1), rgba(4, 202, 255, 1))",
+    green: "linear-gradient(90deg, #90f59a, #04c9ff)",
     purple: "linear-gradient(135deg, #667eea 0%, #764ba2 100%)",
   },
 };
--- src/layout/Loading.tsx
+++ src/layout/Loading.tsx
@@ -1,5 +1,6 @@
-import { Center, Spinner } from "@chakra-ui/react";
+import { Center, Text, VStack } from "@chakra-ui/react";
 import { FC, useState } from "react";
+import { Triangle } from "react-loader-spinner";
 import { usePlayerListener } from "../features/player/hooks/usePlayerListener";
 
 export const Loading: FC = () => {
@@ -13,7 +14,23 @@ export const Loading: FC = () => {
   });
   return isLoading ? (
     <Center bg={"gradation.green"} inset={0} pos={"fixed"} zIndex={"overlay"}>
-      <Spinner color={"white"} size="xl" />
+      <VStack spacing={8}>
+        <Triangle
+          ariaLabel={"triangle-loading"}
+          color={"white"}
+          height={"80"}
+          visible={true}
+          width={"80"}
+        />
+        <Text
+          color={"white"}
+          fontFamily={"barcode"}
+          fontSize={"4xl"}
+          lineHeight={1}
+        >
+          Miku SNS
+        </Text>
+      </VStack>
     </Center>
   ) : null;
 };
