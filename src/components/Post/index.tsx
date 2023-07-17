8eae94142c5a0cc0395e31eeff60684f82e8de3f
tokutsu <tokutsu.beeworks@gmail.com>
2023-07-17 17:20:59 +0900

    ポストの表示のアニメーション

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -8,6 +8,7 @@ import {
   chakra,
   useDisclosure,
 } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import { FC } from "react";
 import { useJotaiAccountId } from "../../features/account/useJotaiAccountId";
 import { accountData } from "../../features/posts/data/accounts";
@@ -31,74 +32,84 @@ export const Post: FC<{
   };
   return (
     <>
-      <Box
-        bg={"gradation.green"}
-        borderRadius={8}
-        color={isActive ? "white" : undefined}
-        id={post.id}
-        p={0.5}
-        {...boxProps}
+      <motion.section
+        animate={"open"}
+        initial={"collapsed"}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+        variants={{
+          open: { height: "auto" },
+          collapsed: { height: 0 },
+        }}
       >
-        <HStack
-          alignItems={"flex-start"}
-          backdropFilter={"blur(12px)"}
-          bg={!isActive ? "white" : undefined}
-          borderRadius={6}
-          justifySelf={"stretch"}
-          key={id}
-          p={3}
+        <Box
+          bg={"gradation.green"}
+          borderRadius={8}
+          color={isActive ? "white" : undefined}
+          id={post.id}
+          p={0.5}
+          {...boxProps}
         >
-          <Avatar
-            as={"button"}
-            bg={"gradation.green"}
-            onClick={() => setAccountId(accountId)}
-            p={0.5}
-            size={"sm"}
-            src={`/images/accounts/${accountId}/icon.jpg`}
-          />
-          <Stack spacing={1} w={"full"}>
-            <HStack justifyContent={"space-between"}>
-              <Text
-                as={"button"}
-                fontSize={"sm"}
-                fontWeight={"bold"}
-                onClick={() => setAccountId(accountId)}
-                textAlign={"left"}
-              >
-                {accountData[accountId].name}
-              </Text>
-              <PostDate date={date} />
-            </HStack>
-            <Stack>
-              <Stack as={"button"} onClick={onOpenDrawer}>
+          <HStack
+            alignItems={"flex-start"}
+            backdropFilter={"blur(12px)"}
+            bg={!isActive ? "white" : undefined}
+            borderRadius={6}
+            justifySelf={"stretch"}
+            key={id}
+            p={3}
+          >
+            <Avatar
+              as={"button"}
+              bg={"gradation.green"}
+              onClick={() => setAccountId(accountId)}
+              p={0.5}
+              size={"sm"}
+              src={`/images/accounts/${accountId}/icon.jpg`}
+            />
+            <Stack spacing={1} w={"full"}>
+              <HStack justifyContent={"space-between"}>
                 <Text
+                  as={"button"}
                   fontSize={"sm"}
+                  fontWeight={"bold"}
+                  onClick={() => setAccountId(accountId)}
                   textAlign={"left"}
-                  whiteSpace={"pre-wrap"}
                 >
-                  {content}
+                  {accountData[accountId].name}
                 </Text>
-                {/* 画像 */}
-                {imageName && (
-                  <chakra.img
-                    alignSelf={"center"}
-                    alt=""
-                    borderRadius={8}
-                    loading={"lazy"}
-                    maxH={isActive ? "200px" : "300px"}
-                    src={`/images/accounts/${accountId}/posts/${imageName}`}
-                  />
-                )}
-              </Stack>
-              {hasPlayer && <PostPlayer />}
-              <HStack spacing={4}>
-                <Likes post={post} />
-                <RepliesButton onOpenDrawer={onOpenDrawer} post={post} />
+                <PostDate date={date} />
               </HStack>
+              <Stack>
+                <Stack as={"button"} onClick={onOpenDrawer}>
+                  <Text
+                    fontSize={"sm"}
+                    textAlign={"left"}
+                    whiteSpace={"pre-wrap"}
+                  >
+                    {content}
+                  </Text>
+                  {/* 画像 */}
+                  {imageName && (
+                    <chakra.img
+                      alignSelf={"center"}
+                      alt=""
+                      borderRadius={8}
+                      loading={"lazy"}
+                      maxH={isActive ? "200px" : "300px"}
+                      src={`/images/accounts/${accountId}/posts/${imageName}`}
+                    />
+                  )}
+                </Stack>
+                {hasPlayer && <PostPlayer />}
+                <HStack spacing={4}>
+                  <Likes post={post} />
+                  <RepliesButton onOpenDrawer={onOpenDrawer} post={post} />
+                </HStack>
+              </Stack>
             </Stack>
-          </Stack>
-        </HStack>
-      </Box>
+          </HStack>
+        </Box>
+      </motion.section>
 
       <RepliesDrawer isOpen={isOpen} onClose={onClose} post={post} />
     </>
