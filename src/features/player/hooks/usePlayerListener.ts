220d6ee31f9efa8aec97246939bbfa636d430c96
tokutsu <tokutsu.beeworks@gmail.com>
2023-10-01 02:06:32 +0900

    不要なコメントを削除

--- src/features/player/hooks/usePlayerListener.ts
+++ src/features/player/hooks/usePlayerListener.ts
@@ -5,7 +5,6 @@ import { useJotaiPlayer } from "../../../jotai/useJotaiPlayer";
 export const usePlayerListener = (listener: PlayerListener) => {
   const { player } = useJotaiPlayer();
 
-  // leftのみを監視
   useLayoutEffect(() => {
     // 解除時にも同じ変数を参照
     const thisListener = listener;
